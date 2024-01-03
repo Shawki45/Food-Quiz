11d9c8356627d1a59c1ff74c37e384baaa16a155
Shawki45 <shawkihaifa@yahoo.com>
2024-01-02 20:21:07 -0500

    added the html header and p and the timer in java

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+var startButtonEL = document.getElementById('Start-Quiz');
+startButtonEL.addEventListener("click", Startquiz); 
+
+var TimerEl = document.getElementById('Timer')
+
+var Seconds = 75 
+
+function Startquiz() {
+    console.log(startButtonEL)
+    TimerEl.textContent = Seconds
+    var Timer = setInterval(function() {
+
+        // Find the distance between now and the count down date
+        Seconds = Seconds - 1;
+        TimerEl.textContent = Seconds  
+          
+    
+          
+        // If the count down is over, write some text 
+         if (distance < 0) {
+           clearInterval(x);
+        
+        }
+      }, 1000);
+
+  }
+  
