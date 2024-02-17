8f8aba9d64be0172fed0ad35f79e2f4333e54019
Shawki45 <shawkihaifa@yahoo.com>
2024-02-17 14:06:17 -0500

    fixed up the readme

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Code-Quiz
 
 ## Description
-    What we have here is an almost code quiz javascript game. We tried to figure how to move on to the next question but found it to be quite difficult. In the future hopefull i will be able to finish. But i did have fun figuring it out.
+    What we have here is an a food quiz javascript game using javascript and html. You will go through 4 questions and answer them but ofcourse the answers that i deemed correct will be correct. We simply click on the start quiz button and the quiz begins with the timer.
 
+
+
+
+## Project Links
+    - 
 
 ## Sources
     -tutors and zoom class meetings
     -https://www.youtube.com/watch?v=hdI2bqOjy3c&t=1622s
     -https://www.w3schools.com/
     -https://developer.mozilla.org/en-US/
+
+
+## Screenshot
+
+![food quiz screebshot](./images/foodquizstart.png)
--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,79 @@
-var startButtonEL = document.getElementById('Start-Quiz');
-startButtonEL.addEventListener("click", Startquiz); 
-var TimerEl = document.getElementById('Timer')
-var Seconds = 75 
-var index = 0
-var Timer
+var startButtonEL = document.getElementById("Start-Quiz");
+startButtonEL.addEventListener("click", Startquiz);
+var TimerEl = document.getElementById("Timer");
+var Seconds = 75;
+var index = 0;
+var Timer;
 var questions = [
   {
     // array containing the quiz questions
-    question: 'Which food is best?',
-    choices: ['Chicken', 'Steak', 'Fish', 'All-the-above'],
-    answer: 'All-the-above'
+    question: "Which food is best?",
+    choices: ["Chicken", "Steak", "Fish", "All-the-above"],
+    answer: "All-the-above",
   },
   {
-    question: 'What is the best kind of snack?',
-    choices: ['Chips', 'Candy', 'Carrots', 'All the above'],
-    answer: 'Carrots'
+    question: "What is the best kind of snack?",
+    choices: ["Chips", "Candy", "Carrots", "All the above"],
+    answer: "Carrots",
   },
   {
-    question: 'How many bars comes with a twix pack?',
-    choices: ['5','4','2','1'],
-    answer: '2'
+    question: "How many bars comes with a twix pack?",
+    choices: ["5", "4", "2", "1"],
+    answer: "2",
   },
   {
-    question: 'What is a good fruit to eat?',
-    choices: ['Mango','Carrots','Apples','Guava'],
-    answer: 'Mango'
-  }
-
-]
+    question: "What is a good fruit to eat?",
+    choices: ["Mango", "Carrots", "Apples", "Guava"],
+    answer: "Mango",
+  },
+];
 // this starts the quiz
 function Startquiz() {
-    console.log(startButtonEL)
-    TimerEl.textContent = Seconds
-    document.querySelector("#Welcome").style.display = 'none'
-    Timer = setInterval(function() {
+  console.log(startButtonEL);
+  TimerEl.textContent = Seconds;
+  document.querySelector("#Welcome").style.display = "none";
+  Timer = setInterval(function () {
+    // Find the distance between now and the count down date
+    Seconds = Seconds - 1;
+    TimerEl.textContent = Seconds;
 
-        // Find the distance between now and the count down date
-        Seconds = Seconds - 1;
-        TimerEl.textContent = Seconds  
-          
-    
-          
-        // If the count down is over, write some text 
-         if (Seconds < 1) {
-           clearInterval(Timer);
-        
-        }
-      }, 1000);
-      displayquestions()
-  }
-  // this displays the questions
+    // If the count down is over, write some text
+    if (Seconds < 1) {
+      clearInterval(Timer);
+    }
+  }, 1000);
+  displayquestions();
+}
+// this displays the questions
 function displayquestions() {
-  document.querySelector("#quiz-questions").innerHTML=""
-  var questionEl = document.createElement("h3")
-  questionEl.textContent = questions[index].question
-  document.querySelector("#quiz-questions").append(questionEl)
+  document.querySelector("#quiz-questions").innerHTML = "";
 
-for (var i = 0; i < questions[index].choices.length;i++) {
-var button = document.createElement("button")
-button.textContent = questions[index].choices[i]
-// add the event listener for each choice button
-button.addEventListener("click", function(event){
-  if (event.target.innerText !== questions[index].answer){
-    Seconds -= 5
+  var questionEl = document.createElement("h3");
+  questionEl.textContent = questions[index].question;
+  document.querySelector("#quiz-questions").append(questionEl);
+
+  for (var i = 0; i < questions[index].choices.length; i++) {
+    var button = document.createElement("button");
+    button.textContent = questions[index].choices[i];
+    // add the event listener for each choice button
+    button.addEventListener("click", function (event) {
+      if (event.target.innerText !== questions[index].answer) {
+        Seconds -= 5;
+      }
+      // moves to the next question
+      index++;
+      if (index >= questions.length) {
+        var score = document.createElement("p");
+        score.innerText = "Your Score Is " + (Seconds - 5);
+        document.querySelector("#quiz-questions").innerHTML = "";
+        document.querySelector("#quiz-end").append(score);
+
+        // stops the timer
+        clearInterval(Timer);
+        return;
+      }
+      displayquestions();
+    });
+    document.querySelector("#quiz-questions").append(button);
   }
-  // moves to the next question
-index ++
-if (index >= questions.length){
-  var score = document.createElement("p")
-  score.innerText = "Your Score Is " + (Seconds -5)
-  document.querySelector("#quiz-questions").innerHTML=""
-  document.querySelector("#quiz-end").append(score)
-
-  // stops the timer
-  clearInterval(Timer);
-  return 
-}
-displayquestions()
-})
-document.querySelector("#quiz-questions").append(button)
-
-}
 }
